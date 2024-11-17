2b638b27614ca152193e0c76760389caf14b9eaa
abhikr02 <abhishek.kr.git02@gmail.com>
2024-11-17 11:49:40 +0530

    Updated the second highest value logic

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb+srv://mdnurullahcse2021:CIQsV86iydk6TrnA@cluster0.ovr60.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://mdnurullahcse2021:7Acx7l2iN2RSgwl8@naanmudhalavanproject.cg7oi.mongodb.net/?retryWrites=true&w=majority&appName=NaanMudhalavanProject")
 .then(()=>{
    console.log("connected to mongodb")
 })
